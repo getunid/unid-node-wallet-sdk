27817fe307052659da66a58db8c5e838af081fc3
shotaro chiba <s@pws.jp>
2021-01-14 06:29:59 +0900

    feat: Added schemas

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,14 @@ import { MongoDBClient } from './adapters/mongodb'
 import { MongoDBConnector } from './connector/mongodb'
 import { KeyRingType } from './keyring'
 import { AddressCredentialV1 } from './schemas/address'
-// import {
-//     AddressCredentialV1,
-//     AlumniOfCredentialV1,
-//     BirthDateCredentialV1,
-//     ContactPointCredentialV1,
-//     EmailCredentialV1,
-//     GenderCredentialV1,
-//     NameCredentialV1,
-//     PhoneCredentialV1,
-//     QualificationCredentialV1,
-// } from './schemas'
+import { AlumniOfCredentialV1 } from './schemas/alumni-of'
+import { BirthDateCredentialV1 } from './schemas/birth-date'
+import { ContactPointCredentialV1 } from './schemas/contact-point'
+import { EmailCredentialV1 } from './schemas/email'
+import { GenderCredentialV1 } from './schemas/gender'
+import { NameCredentialV1 } from './schemas/name'
+import { PhoneCredentialV1 } from './schemas/phone'
+import { QualificationCredentialV1 } from './schemas/qualification'
 
 export {
     UNiD,
@@ -22,12 +19,12 @@ export {
     MongoDBConnector,
     KeyRingType,
     AddressCredentialV1,
-    // AlumniOfCredentialV1,
-    // BirthDateCredentialV1,
-    // ContactPointCredentialV1,
-    // EmailCredentialV1,
-    // GenderCredentialV1,
-    // NameCredentialV1,
-    // PhoneCredentialV1,
-    // QualificationCredentialV1,
+    AlumniOfCredentialV1,
+    BirthDateCredentialV1,
+    ContactPointCredentialV1,
+    EmailCredentialV1,
+    GenderCredentialV1,
+    NameCredentialV1,
+    PhoneCredentialV1,
+    QualificationCredentialV1,
 }
